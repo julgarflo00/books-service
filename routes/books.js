2ea8851a7a57c8f9c6461ec3935c9c3221f8e779
julgarflo00 <julgarflo@alum.us.es>
2023-12-01 19:27:30 +0100

    Ading books

--- routes/books.js
+++ routes/books.js
@@ -2,141 +2,191 @@ var express = require('express');
 var router = express.Router();
 
 var books =[
-  {"id": 1, 
-  "title" :"Pride and Prejudice", 
-  "author":"Jane Austen", 
-  "year":"1813", 
-  "genre":"romance",
-  "options":[{"seller": 1, "stock":100, "prize":9.9 },
-  {"seller": 2, "stock":120, "prize":12.10 } 
-]
-},
-  {"id": 2, 
-  "title" :"Harry Potter and the philosopher's stone", 
-  "author":"J.K.Rowling", 
-  "year":"1997", 
-  "genre":"fantasy", 
-  "options":[{"seller": 2, "stock":110, "prize":9.9 },
-  {"seller": 2, "stock":120, "prize":12.10 } 
-]
-},
-  {"id": 3, 
-  "title":"Harry Potter and the chamber of secrets", 
-  "author":"J.K.Rowling", 
-  "year":"1998", 
-  "genre":"fantasy", 
-  "options":[{"seller": 3, "stock":102, "prize":9.9 },
-  {"seller": 2, "stock":120, "prize":12.10 } 
-]
-},
+  {
+    "id": 1,
+    "title": "Harry Potter y la piedra filosofal",
+    "author": "J.K.Rowling",
+    "year": "1997",
+    "genre": "fantasía",
+    "options": [
+      { "seller": 2, "stock": 110, "prize": 9.9 },
+      { "seller": 2, "stock": 120, "prize": 12.10 }
+    ]
+  },
+  {
+    "id": 2,
+    "title": "Harry Potter y la cámara secreta",
+    "author": "J.K.Rowling",
+    "year": "1998",
+    "genre": "fantasía",
+    "options": [
+      { "seller": 3, "stock": 105, "prize": 10.5 },
+      { "seller": 2, "stock": 115, "prize": 13.20 }
+    ]
+  },
+  {
+    "id": 3,
+    "title": "Harry Potter y el prisionero de Azkaban",
+    "author": "J.K.Rowling",
+    "year": "1999",
+    "genre": "fantasía",
+    "options": [
+      { "seller": 4, "stock": 100, "prize": 11.0 },
+      { "seller": 3, "stock": 110, "prize": 14.50 }
+    ]
+  },
   {
     "id": 4,
-    "title": "To Kill a Mockingbird",
-    "author": "Harper Lee",
-    "year": "1960",
-    "genre": "fiction",
+    "title": "Harry Potter y el cáliz de fuego",
+    "author": "J.K.Rowling",
+    "year": "2000",
+    "genre": "fantasía",
     "options": [
-      {"seller": 1, "stock": 90, "prize": 10.5},
-      {"seller": 3, "stock": 80, "prize": 11.75}
+      { "seller": 1, "stock": 95, "prize": 11.5 },
+      { "seller": 4, "stock": 105, "prize": 15.0 }
     ]
   },
   {
     "id": 5,
-    "title": "1984",
-    "author": "George Orwell",
-    "year": "1949",
-    "genre": "dystopian",
+    "title": "Harry Potter y la Orden del Fénix",
+    "author": "J.K.Rowling",
+    "year": "2003",
+    "genre": "fantasía",
     "options": [
-      {"seller": 2, "stock": 95, "prize": 12.99},
-      {"seller": 3, "stock": 110, "prize": 14.25}
+      { "seller": 2, "stock": 90, "prize": 12.0 },
+      { "seller": 1, "stock": 100, "prize": 16.0 }
     ]
   },
   {
     "id": 6,
-    "title": "The Great Gatsby",
-    "author": "F. Scott Fitzgerald",
-    "year": "1925",
-    "genre": "fiction",
+    "title": "Harry Potter y el misterio del príncipe",
+    "author": "J.K.Rowling",
+    "year": "2005",
+    "genre": "fantasía",
     "options": [
-      {"seller": 1, "stock": 120, "prize": 11.2},
-      {"seller": 3, "stock": 100, "prize": 13.45}
+      { "seller": 3, "stock": 85, "prize": 12.5 },
+      { "seller": 4, "stock": 95, "prize": 17.0 }
     ]
   },
   {
     "id": 7,
-    "title": "The Catcher in the Rye",
-    "author": "J.D. Salinger",
-    "year": "1951",
-    "genre": "fiction",
+    "title": "Harry Potter y las Reliquias de la Muerte",
+    "author": "J.K.Rowling",
+    "year": "2007",
+    "genre": "fantasía",
     "options": [
-      {"seller": 2, "stock": 85, "prize": 10.75},
-      {"seller": 1, "stock": 95, "prize": 12.0}
+      { "seller": 1, "stock": 80, "prize": 13.0 },
+      { "seller": 2, "stock": 90, "prize": 18.0 }
     ]
   },
   {
-    "id": 8,
-    "title": "The Lord of the Rings",
-    "author": "J.R.R. Tolkien",
-    "year": "1954",
-    "genre": "fantasy",
+    "id": 1,
+    "title": "Orgullo y prejuicio",
+    "author": "Jane Austen",
+    "year": "1813",
+    "genre": "romance",
     "options": [
-      {"seller": 3, "stock": 110, "prize": 14.5},
-      {"seller": 1, "stock": 105, "prize": 16.0}
+      { "seller": 1, "stock": 100, "prize": 9.9 },
+      { "seller": 2, "stock": 120, "prize": 12.10 }
     ]
   },
   {
-    "id": 9,
-    "title": "Brave New World",
-    "author": "Aldous Huxley",
-    "year": "1932",
-    "genre": "dystopian",
+    "id": 2,
+    "title": "Amor en los tiempos del cólera",
+    "author": "Gabriel García Márquez",
+    "year": "1985",
+    "genre": "romance",
     "options": [
-      {"seller": 1, "stock": 100, "prize": 13.0},
-      {"seller": 2, "stock": 115, "prize": 15.25}
+      { "seller": 3, "stock": 95, "prize": 10.5 },
+      { "seller": 4, "stock": 110, "prize": 14.0 }
     ]
   },
   {
-    "id": 10,
-    "title": "The Hobbit",
-    "author": "J.R.R. Tolkien",
-    "year": "1937",
-    "genre": "fantasy",
+    "id": 3,
+    "title": "Romeo y Julieta",
+    "author": "William Shakespeare",
+    "year": "1597",
+    "genre": "romance",
     "options": [
-      {"seller": 2, "stock": 95, "prize": 11.9},
-      {"seller": 3, "stock": 90, "prize": 13.15}
+      { "seller": 2, "stock": 80, "prize": 11.0 },
+      { "seller": 1, "stock": 90, "prize": 13.0 }
     ]
   },
   {
-    "id": 11,
-    "title": "Fahrenheit 451",
-    "author": "Ray Bradbury",
-    "year": "1953",
-    "genre": "dystopian",
+    "id": 4,
+    "title": "Cumbres borrascosas",
+    "author": "Emily Brontë",
+    "year": "1847",
+    "genre": "romance",
     "options": [
-      {"seller": 1, "stock": 105, "prize": 12.8},
-      {"seller": 3, "stock": 88, "prize": 14.0}
+      { "seller": 4, "stock": 85, "prize": 12.5 },
+      { "seller": 3, "stock": 95, "prize": 17.0 }
     ]
   },
   {
-    "id": 12,
-    "title": "The Chronicles of Narnia",
-    "author": "C.S. Lewis",
-    "year": "1950",
-    "genre": "fantasy",
+    "id": 5,
+    "title": "Diario de una pasión",
+    "author": "Nicholas Sparks",
+    "year": "1996",
+    "genre": "romance",
     "options": [
-      {"seller": 2, "stock": 98, "prize": 11.5},
-      {"seller": 1, "stock": 92, "prize": 13.75}
+      { "seller": 1, "stock": 70, "prize": 14.0 },
+      { "seller": 2, "stock": 80, "prize": 18.0 }
     ]
   },
   {
-    "id": 13,
-    "title": "The Grapes of Wrath",
-    "author": "John Steinbeck",
-    "year": "1939",
-    "genre": "fiction",
+    "id": 1,
+    "title": "El código Da Vinci",
+    "author": "Dan Brown",
+    "year": "2003",
+    "genre": "thriller",
     "options": [
-      {"seller": 3, "stock": 115, "prize": 10.99},
-      {"seller": 1, "stock": 100, "prize": 12.25}
+      { "seller": 1, "stock": 90, "prize": 11.5 },
+      { "seller": 2, "stock": 100, "prize": 15.0 }
+    ]
+  },
+  {
+    "id": 2,
+    "title": "El silencio de los corderos",
+    "author": "Thomas Harris",
+    "year": "1988",
+    "genre": "thriller",
+    "options": [
+      { "seller": 3, "stock": 80, "prize": 12.0 },
+      { "seller": 4, "stock": 90, "prize": 16.0 }
+    ]
+  },
+  {
+    "id": 3,
+    "title": "Perdida",
+    "author": "Gillian Flynn",
+    "year": "2012",
+    "genre": "thriller",
+    "options": [
+      { "seller": 2, "stock": 75, "prize": 13.0 },
+      { "seller": 1, "stock": 85, "prize": 17.0 }
+    ]
+  },
+  {
+    "id": 4,
+    "title": "El psicoanalista",
+    "author": "John Katzenbach",
+    "year": "2002",
+    "genre": "thriller",
+    "options": [
+      { "seller": 4, "stock": 70, "prize": 14.5 },
+      { "seller": 3, "stock": 80, "prize": 19.0 }
+    ]
+  },
+  {
+    "id": 5,
+    "title": "La chica del tren",
+    "author": "Paula Hawkins",
+    "year": "2015",
+    "genre": "thriller",
+    "options": [
+      { "seller": 1, "stock": 65, "prize": 15.0 },
+      { "seller": 2, "stock": 75, "prize": 20.0 }
     ]
   }
 ]
